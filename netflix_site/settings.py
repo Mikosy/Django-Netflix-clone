6c816893ebaf40b4d94cbaa4782260835252e7d4
Mikosy <miibewuba@gmail.com>
2023-12-23 03:00:01 +0100

    Netflix Clone

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'netflix_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Using fl0 postgresql database
 # DATABASES = {
 #     'default': {
-#         'ENGINE':'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'database',
-#         'USER': 'fl0user',
-#         'PASSWORD': 'jxqbW9IgXNn1',
-#         'HOST': 'ep-gentle-rain-63461340.ap-southeast-1.aws.neon.fl0.io',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# Using fl0 postgresql database
+DATABASES = {
+    'default': {
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'fl0user',
+        'PASSWORD': 'jxqbW9IgXNn1',
+        'HOST': 'ep-gentle-rain-63461340.ap-southeast-1.aws.neon.fl0.io',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
